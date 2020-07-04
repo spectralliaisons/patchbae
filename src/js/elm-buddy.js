fa8a1f57ee7b653734badd819296d7efc70dcf0e
Wesley Jackson <wes@wesmjackson.com>
2020-07-04 13:26:59 -0700

    verify the right keys for uid and patches

--- src/js/elm-buddy.js
+++ src/js/elm-buddy.js
@@ -30,24 +30,10 @@ Elm ports
         };
 
         // Attempt to load remote table for user credentials
-        // https://tutorialedge.net/projects/building-blog-with-vuejs-aws/part-5-getting-started-with-cognito/
         var authenticate = function ([username, password]) {
-            
-            // TODO: Amazon cognito to preserve login state
-            // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentity.html
-            // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityCredentials.html
             
             // defined at ./src/js/priv/cognitoConfig.js
             var config = configureCognito();
-            // var userPool = new AmazonCognitoIdentity.CognitoUserPool(config);
-            // var cognitoUser = userPool.getCurrentUser();
-
-            // var userPool = new AmazonCognitoIdentity.CognitoUserPool(config);
-            // let authenticationData = { Username: username, Password: password };
-
-            // let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData)
-            // let userData = { Username: username, Pool: userPool }
-            // let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData)
             
             var sendError = function(err) {
                 
@@ -57,65 +43,9 @@ Elm ports
                 app.ports.handle_authentication.send("");
             }
             
+            // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentity.html
+            // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityCredentials.html
             // https://aws.amazon.com/blogs/mobile/building-fine-grained-authorization-using-amazon-cognito-user-pools-groups/
-            // try {
-            //     if (cognitoUser != null) {
-            //         cognitoUser.getSession(function(err, session) {
-
-            //             if (!err) {
-            //                 console.log('session validity: ' + session.isValid());
-            //                 console.log('session token: ' + session.getIdToken().getJwtToken());
-    
-            //                 var loginDat = {};
-            //                 loginDat['cognito-idp.' + config.region + '.amazonaws.com/' + config.UserPoolId] = session.getIdToken().getJwtToken();
-                            
-            //                 AWS.config.region = config.region;
-            //                 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            //                     IdentityPoolId : config.IdentityPoolId, 
-            //                     Logins : loginDat
-            //                 });
-
-            //                 AWS.config.credentials.get(function(err) {
-            //                     if (!err) {
-
-            //                         var uid = AWS.config.credentials.identityId;
-            //                         console.log('Cognito Identity ID '+ uid);
-                                    
-            //                         // Instantiate aws sdk service objects now that the credentials have been updated
-            //                         var docClient = new AWS.DynamoDB.DocumentClient({ region: AWS.config.region });
-            //                         var params = {
-            //                             TableName: "Patchbae",
-            //                             Key: {"UID": uid}
-            //                         };
-            //                         docClient.get(params, function(err, data) {
-            //                             if (err || !data || data.Item == undefined) {
-            //                                 // Initialize a data table for this user
-            //                                 // var res = {uid:uid, patches:[]};
-            //                                 var res = _.extend(pb_demo(), {uid:uid});
-            //                                 app.ports.handle_authentication.send(JSON.stringify(res));
-            //                             } else {
-            //                                 var userData = {uid: data.Item.UID, patches:data.Item.Patches};
-            //                                 var res = JSON.stringify(userData);
-                                            
-            //                                 app.ports.handle_authentication.send(res);
-            //                             }
-            //                         });
-            //                     }
-            //                     else {
-            //                         sendError(err);
-            //                     }
-            //                 });
-            //             }
-            //             else {
-            //                 sendError(err);
-            //             }
-            //         });
-            //     }
-            // }
-            // catch (err) {
-            //     sendError(err);
-            // }
-            
             // https://tutorialedge.net/projects/building-blog-with-vuejs-aws/part-5-getting-started-with-cognito/
             var userPool = new AmazonCognitoIdentity.CognitoUserPool(config);
             let authenticationData = { Username: username, Password: password };
@@ -161,8 +91,10 @@ Elm ports
                                 if (err || !data || data.Item == undefined) {
                                     
                                     // Initialize a data table for this user
-                                    // var res = {uid:uid, patches:[]};
-                                    var res = _.extend(pb_demo(), {uid:uid});
+                                    var res = {uid:uid, patches:[]};
+
+                                    // use this instead to use my real patches for a user with no data
+                                    // var res = _.extend(pb_demo(), {uid:uid});
 
                                     // remember this authentication token and user data
                                     var curr = load();
@@ -194,32 +126,6 @@ Elm ports
                     sendError({});
                 }
             });
-
-            // AWS.config.update({
-            //     region: "us-west-1",
-            //     accessKeyId: "AKIARMA57WRKW54DBSY3", // TODO: how to not have this in code?
-            //     secretAccessKey: password
-            // });
-
-            // var params = {
-            //     TableName: "Patchbae",
-            //     Key: {"UID": uid}
-            // };
-
-            // var docClient = new AWS.DynamoDB.DocumentClient();
-
-            // docClient.get(params, function(err, data) {
-            //     if (err || data.Item == undefined) {
-            //         console.log("Error authenticating:");
-            //         console.log(JSON.stringify(err, undefined, 2));
-                    
-            //         app.ports.handle_authentication.send("");
-            //     } else {
-            //         var userData = {uid: data.Item.UID, patches:data.Item.Patches};
-            //         var res = JSON.stringify(userData);
-            //         app.ports.handle_authentication.send(res);
-            //     }
-            // });
         };
         
         // Save UserData in local storage and remotely if user is logged in
@@ -237,8 +143,8 @@ Elm ports
                 curr.token = null;
             }
             localStorage.setItem(key, JSON.stringify(_.extend(curr, {data:rawDat})));
-
-            if (curr.token && dat.uid && dat.uid != "anonymous") {
+            
+            if (rawDat.uid != null && rawDat.uid != "anonymous") {
 
                 var token = curr.token;
                 
